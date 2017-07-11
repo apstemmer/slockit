3e86b891b71f5f2b18b522647e1fa84db46b89ef
Arthur Stemmer <apstemmer@hotmail.com>
2017-07-10 21:18:36 -0700

    renaming some things

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-var path = require('path');
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname)));
+// Define the port to run on
+app.set('port', 80);
 
-app.get('/',(req, res)=>{
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static(path.join(__dirname, '/')));
 
-app.listen(3000, function(){
-  console.log("started listening on port 3000");
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
 });
